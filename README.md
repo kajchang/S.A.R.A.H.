80a64c39ff95fe7e56b4c8d42daf8ef4f00a78e6
Kai Chang <kaijchang@gmail.com>
2018-08-08 12:18:57 -0700

    more commands
    !info, !nvt, and !play commands

--- README.md
+++ README.md
@@ -2,9 +2,29 @@
 
 A Discord Bot for the [NovaBlitz](https://novablitz.com/) card game.
 
-## Commands
+## Available Commands
+
+<img src="https://github.com/kajchang/SARAH/raw/master/images/!card.png" height="250"/>
 
 `!card card-name`
 
-<img src="https://github.com/kajchang/SARAH/raw/master/assets/!card.png" width="200" height="200" />
-Fetches a NovaBlitz card by name.
+Searches for a card using the [NovaBlitz API](https://documenter.getpostman.com/view/4967569/RWTivyzL).
+
+<img src="https://github.com/kajchang/SARAH/raw/master/images/!nvt.png" height="250"/>
+
+`!nvt`
+
+Gets the latest price for the Nova Token using the [CoinGecko API](https://www.coingecko.com/api/docs/v3).
+
+<img src="https://github.com/kajchang/SARAH/raw/master/images/!play.png" height="250"/>
+
+`!play`
+
+Displays links to the Steam page for NovaBlitz and the signup form for the Mobile Beta.
+
+
+<img src="https://github.com/kajchang/SARAH/raw/master/images/!info.png" height="250"/>
+
+`!info`
+
+Displays a link to this repository, a way to contact the author, and an invite link for the bot.
--- cogs/SARAH.py
+++ cogs/SARAH.py
@@ -9,7 +9,7 @@ class SARAH:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True, description='Search for information on a card.')
+    @commands.command(pass_context=True, description='Searches for a card')
     async def card(self, ctx, *args):
         async with aiohttp.ClientSession() as session:
             async with session.get('http://novablitz.pythonanywhere.com/cards/{}'.format(''.join(arg for arg in args))) as response:
@@ -18,11 +18,53 @@ class SARAH:
 
                 else:
                     card_data = await response.json()
-                    async with aiohttp.ClientSession() as session:
-                        async with session.get(card_data['image']) as response:
-                            image = BytesIO(await response.read())
+                    async with session.get(card_data['image']) as response:
+                        image = BytesIO(await response.read())
 
                     await self.bot.send_file(ctx.message.channel, fp=image, filename='{}.png'.format(card_data['name']))
+
+    @commands.command(description='Information on Nova Token')
+    async def nvt(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://api.coingecko.com/api/v3/coins/nova-token') as response:
+                token_data = await response.json()
+                change = float(token_data['market_data']
+                               ['price_change_percentage_24h'])
+                embed = discord.Embed(
+                    title='Nova Token', type='rich', color=discord.Colour(0x40e0d0))
+                embed.add_field(name='Price', value='${0:f}'.format(token_data['market_data']
+                                                                    ['current_price']['usd']))
+                embed.add_field(name='24h Change', value='{0:f}% {1}'.format(
+                    change, ':chart_with_downwards_trend:' if change < 0 else ':chart_with_upwards_trend:'))
+                embed.add_field(
+                    name='Info', value='[Token Information](https://novablitz.com/token-sale/)')
+                embed.add_field(
+                    name='Exchanges', value='Buy on [Radar Relay](https://app.radarrelay.com/NVT/WETH) and [ForkDelta](https://forkdelta.github.io/#!/trade/NVT-ETH)')
+
+                await self.bot.say(embed=embed)
+
+    @commands.command(description='Links for Playing NovaBlitz')
+    async def play(self):
+        embed = discord.Embed(title='How to Get NovaBlitz',
+                              type='rich', color=discord.Colour(0x40e0d0))
+        embed.add_field(
+            name='Steam', value='[Download for Free on Steam](http://store.steampowered.com/app/388370)')
+        embed.add_field(
+            name='Mobile Beta', value='[Sign Up for the Mobile Beta](https://goo.gl/forms/yC7tMdlERUctx2d53)', inline=False)
+
+        await self.bot.say(embed=embed)
+
+    @commands.command(description='Information on this bot')
+    async def info(self):
+        embed = discord.Embed(title='Bot Information',
+                              type='rich', color=discord.Colour(0x40e0d0))
+        embed.add_field(name='Author', value='<@288851267847979010>')
+        embed.add_field(
+            name='Github Repo', value='[Bot Repository](https://github.com/kajchang/S.A.R.A.H.)')
+        embed.add_field(name='Add to Your Server',
+                        value='[Invite Me](https://discordapp.com/oauth2/authorize?client_id=476208085246017536&scope=bot&permissions=0)')
+
+        await self.bot.say(embed=embed)
 
 
 def setup(bot):
